6949a42dc88c7c69668f2ab6d59c471d5642960c
x11 <x11@a439b4bb-83d8-46c6-86ec-f6d4d22dddae>
2008-03-15 15:33:30 +0000

    Simple plugin for slug creation and validation.
    git-svn-id: svn://arturaz.net/public/rails/plugins/validates_slugization_of@543 a439b4bb-83d8-46c6-86ec-f6d4d22dddae

---
+++ lib/arturaz/validates_slugization_of.rb
@@ -0,0 +1,51 @@
+module Arturaz
+  module ValidatesSlugizationOf
+    # Create and validate slug for attribute.
+    #
+    # Sets up validation filter to create slug for _attr_. Also checks if that
+    # slug is unique and adds error to _attr_ if it isn't. Stores slug in _slug_
+    # attribute by default.
+    #
+    # Options:
+    # - :to => attribute name to slugize to.
+    # 
+    # It also takes all regular validation and #validates_each options.
+    # 
+    # It can be given a block to slugize value of _attr_ instead of just calling
+    # #slugize on _value_. Block receives two arguments: _record_ and _value_.
+    # Look into #validates_each to learn more about those.
+    # 
+    # Example:
+    # <code>
+    # # Creates a slug from _title_ to _title_slug_ by taking _record_ id and 
+    # # appending it with even or odd.
+    # validates_slugization_of(:title, 
+    #   :to => :title_slug, 
+    #   :on => :create,
+    #   :message => "should be unique."
+    # ) do |record, value|
+    #   "#{record.id}-" + (value % 2 == 0 ? "even" : "odd")
+    # end
+    # </code>
+    #
+    def validates_slugization_of(attr, options={}, &block)
+      options[:to] ||= :slug
+      options[:message] ||= "is already taken."
+
+      validates_each(attr, options) do |record, attr, value|
+        if block.nil?
+          slug = value.slugize
+        else
+          slug = block.call(record, value)
+        end
+
+        record.send "#{options[:to]}=", slug
+        if record.class.find(:first, :conditions => [
+          "#{options[:to]}=? AND #{primary_key}!=?", slug, record.id
+        ])
+          record.errors.add attr, options[:message]
+        end
+      end
+    end
+  end
+end
